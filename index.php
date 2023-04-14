367502aba1e7b5c7576d310073c04cedc5802257
Qrcis <103064730+Qrcis@users.noreply.github.com>
2023-04-14 10:14:35 +0330

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  <body bgcolor='#FFEBCD'>
   <center> 
    <h1>🎉 Congratulations | You did it 😃👏</h1>
-   <p>🔷 To Run Your Robot, Do The Following Stuff 🔶</p>
+   <p>🔷 To Run Your Robot,Do The Following Stuff 🔶</p>
    <p>1️⃣ Edit The <code>"bot.php"</code> File in Your Github That Forked From My Repo and Put Your Code in it.</p>
    <p>2️⃣ Set Your Bot Webhook With This address : "https://api.telegram.org/bot<code>[TOKEN]</code>/setwebhook?url=<code>[DOMAIN]</code>/bot.php"</p>
    <h4>🔎 Hint » Use Your Token instead of <code>[TOKEN]</code> and Your Site address instead of <code>[DOMAIN]</code></h4>
